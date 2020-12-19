f36bb82c2524448df3728e4f8763f9c85f526357
aleger200326 <72569842+aleger200326@users.noreply.github.com>
2020-12-19 03:24:48 +0300

    ромбы

--- Source.cpp
+++ Source.cpp
@@ -1,59 +1,43 @@
 #include <bits/stdc++.h>
-#include <cstdlib>
-#define mp make_pair
-#define f first
-#define s second
-#define sz(x) (int)(x).size()
-#define rsz resize
-#define ins insert
-#define ft front()
-#define bk back()
-#define pf push_front
-#define pb push_back
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> pi;
-typedef pair<ll,ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-int digits(int n)
-{
-	int k=0;
-	while(n>0)
-	{
-		n/=10;
-		k++;
-	}
-	return k;
-}
 int main()
 {
-	srand(time(NULL));
-	int n=5,m=6; 
-	int d=0;
-	int arr[n][m]; 
-	for (int i=0;i<n;i++)for (int j=0;j<m;j++)arr[i][j]=rand()%10000;
-	for (int i=0;i<n;i++)
+	setlocale(LC_ALL,"Rus");
+	int num=0;
+	cout << "¬ведите какое количество ромбов вы хотите увидеть на экране: ";
+	cin >> num;
+	int tmp=num;
+	while(num>0)
 	{
-		for (int j=0;j<m;j++)
-		{
-			d=6-digits(arr[i][j]);
-			cout << arr[i][j];
-			for (;d>0;d--)cout << " ";
-		}
-		cout << "\n";
+		cout << "  *  ";
+		num--;
 	}
 	cout << "\n";
-	for (int i=0;i<n;i++)
+	num=tmp;
+	while(num>0)
 	{
-		for (int j=0;j<m;j++)
-		{
-			d=6-digits(arr[i][j]);
-			if(j!=0)for (;d>0;d--)cout << " ";
-			cout << arr[i][j];
-		}
-		cout << "\n";
+		cout << " *** ";
+		num--;
+	}
+	cout << "\n";
+	num=tmp;
+	while(num>0)
+	{
+		cout << "*****";
+		num--;
+	}
+	cout << "\n";
+	num=tmp;
+	while(num>0)
+	{
+		cout << " *** ";
+		num--;
+	}
+	cout << "\n";
+	num=tmp;
+	while (num>0)
+	{
+		cout << "  *  ";
+		num--;
 	}
 }
